32339ccd152f38560ba563b677501253840618a3
Luis Forra <luis.forra@pdmfc.com>
2019-12-16 22:51:24 +0000

    added the css main styles file and added the initiative funtionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("connected");
-
 let party = [
   {
     name: "cenas",
@@ -78,7 +76,7 @@ let enemies = [
   }
 ];
 
-function createCharacterElement(character) {
+function createCharacterElement(character, index) {
   let liElement = document.createElement("li");
   liElement.classList.add("character");
 
@@ -95,10 +93,17 @@ function createCharacterElement(character) {
   initiativeContainerElement.classList.add("initiative-container");
 
   let initiativeCounterElement = document.createElement("span");
+  initiativeCounterElement.classList.add("initiative-counter");
+  initiativeCounterElement.dataset.indexNumber = `${index}`;
   let initiativeValue = document.createTextNode(`${character.iniciative}`);
+
+  let initiativeInputElement = document.createElement("input");
+  initiativeInputElement.classList.add("initiative-input", "hide");
+
   initiativeCounterElement.appendChild(initiativeValue);
 
   initiativeContainerElement.appendChild(initiativeCounterElement);
+  initiativeContainerElement.appendChild(initiativeInputElement);
 
   let nameValue = document.createTextNode(`${character.name}`);
   nameElement.appendChild(nameValue);
@@ -119,18 +124,80 @@ function createCharacterElement(character) {
 function addPartyList() {
   let partyUListElement = document.querySelector(".party-list");
 
-  party.forEach(function(character) {
-    partyUListElement.appendChild(createCharacterElement(character));
+  party.forEach(function(character, index) {
+    partyUListElement.appendChild(createCharacterElement(character, index));
   });
 }
 
 function addEnemyList() {
   let enemyUListElement = document.querySelector(".enemies-list");
 
-  enemies.forEach(function(enemies) {
-    enemyUListElement.appendChild(createCharacterElement(enemies));
+  enemies.forEach(function(enemies, index) {
+    enemyUListElement.appendChild(createCharacterElement(enemies, index));
+  });
+}
+
+function handleEventListeners() {
+  const partyListContainer = document.querySelector(".party-container");
+  const enemyListContainer = document.querySelector(".enemies-container");
+
+  // Toggle span to input party list
+  partyListContainer.addEventListener("dblclick", function(event) {
+    if (event.target.className === "initiative-counter") {
+      event.target.classList.add("hide");
+      event.target.nextElementSibling.classList.remove("hide");
+    }
+  });
+
+  // Toggle span to input enemies list
+  enemyListContainer.addEventListener("dblclick", function(event) {
+    if (event.target.className === "initiative-counter") {
+      event.target.classList.add("hide");
+      event.target.nextElementSibling.classList.remove("hide");
+    }
+  });
+
+  enemyListContainer.addEventListener("keypress", function(event) {
+    if (event.target.className === "initiative-input") {
+      let key = event.which || event.keyCode;
+      let inputElement = event.target;
+
+      if (key === 13) {
+        // on enter get the input value
+        let inputValue = inputElement.value === "" ? 0 : inputElement.value;
+        let spanElement = inputElement.previousSibling;
+
+        // add the value in the span
+        spanElement.innerHTML = inputValue;
+
+        // toggle
+        inputElement.classList.add("hide");
+        spanElement.classList.remove("hide");
+      }
+    }
+  });
+
+  partyListContainer.addEventListener("keypress", function(event) {
+    if (event.target.className === "initiative-input") {
+      let key = event.which || event.keyCode;
+      let inputElement = event.target;
+
+      if (key === 13) {
+        // on enter get the input value
+        let inputValue = inputElement.value === "" ? 0 : inputElement.value;
+        let spanElement = inputElement.previousSibling;
+
+        // add the value in the span
+        spanElement.innerHTML = inputValue;
+
+        // toggle
+        inputElement.classList.add("hide");
+        spanElement.classList.remove("hide");
+      }
+    }
   });
 }
 
 addPartyList();
 addEnemyList();
+handleEventListeners();
