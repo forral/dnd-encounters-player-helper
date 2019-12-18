59addcbd6b11f2ed6c59041e7132bbd24e0edd08
Luis Forra <luis.forra@pdmfc.com>
2019-12-18 00:07:51 +0000

    refactor toggle functionality

--- script.js
+++ script.js
@@ -137,70 +137,56 @@ function addEnemyList() {
   });
 }
 
+function toggleSpanToInput(element) {
+  element.classList.add("hide");
+  element.nextElementSibling.classList.remove("hide");
+  element.nextElementSibling.focus();
+}
+
+function toggleInputToSpan(event, array) {
+  let key = event.which || event.keyCode;
+  let inputElement = event.target;
+
+  if (key !== 13) return;
+
+  // on enter get the input value
+  let inputValue = inputElement.value === "" ? 0 : inputElement.value;
+  let spanElement = inputElement.previousSibling;
+
+  // add the value in the span
+  spanElement.innerHTML = inputValue;
+
+  // toggle
+  inputElement.classList.add("hide");
+  spanElement.classList.remove("hide");
+
+  array[spanElement.dataset.indexNumber].iniciative = inputValue;
+}
+
 function handleEventListeners() {
   const partyListContainer = document.querySelector(".party-container");
   const enemyListContainer = document.querySelector(".enemies-container");
 
   // Toggle span to input party list
   partyListContainer.addEventListener("dblclick", function(event) {
-    if (event.target.className === "initiative-counter") {
-      event.target.classList.add("hide");
-      event.target.nextElementSibling.classList.remove("hide");
-      event.target.nextElementSibling.focus();
-    }
+    if (event.target.className !== "initiative-counter") return;
+    toggleSpanToInput(event.target);
   });
 
   // Toggle span to input enemies list
   enemyListContainer.addEventListener("dblclick", function(event) {
-    if (event.target.className === "initiative-counter") {
-      event.target.classList.add("hide");
-      event.target.nextElementSibling.classList.remove("hide");
-      event.target.nextElementSibling.focus();
-    }
-  });
-
-  enemyListContainer.addEventListener("keypress", function(event) {
-    if (event.target.className === "initiative-input") {
-      let key = event.which || event.keyCode;
-      let inputElement = event.target;
-
-      if (key !== 13) return;
-
-      // on enter get the input value
-      let inputValue = inputElement.value === "" ? 0 : inputElement.value;
-      let spanElement = inputElement.previousSibling;
-
-      // add the value in the span
-      spanElement.innerHTML = inputValue;
-
-      // toggle
-      inputElement.classList.add("hide");
-      spanElement.classList.remove("hide");
-
-      enemies[spanElement.dataset.indexNumber].iniciative = inputValue;
-    }
+    if (event.target.className !== "initiative-counter") return;
+    toggleSpanToInput(event.target);
   });
 
   partyListContainer.addEventListener("keypress", function(event) {
-    if (event.target.className === "initiative-input") {
-      let key = event.which || event.keyCode;
-      let inputElement = event.target;
+    if (event.target.className !== "initiative-input") return;
+    toggleInputToSpan(event, party);
+  });
 
-      if (key !== 13) return;
-
-      // on enter get the input value
-      let inputValue = inputElement.value === "" ? 0 : inputElement.value;
-      let spanElement = inputElement.previousSibling;
-
-      // add the value in the span
-      spanElement.innerHTML = inputValue;
-
-      // toggle
-      inputElement.classList.add("hide");
-      spanElement.classList.remove("hide");
-
-      party[spanElement.dataset.indexNumber].iniciative = inputValue;
-    }
+  enemyListContainer.addEventListener("keypress", function(event) {
+    if (event.target.className !== "initiative-input") return;
+    toggleInputToSpan(event, enemies);
   });
 }
 
