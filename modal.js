f2c91ceba0312abf589d8836f5d6f6daf5209148
Luis Forra <luis.forra@pdmfc.com>
2019-12-22 23:25:18 +0000

    modal title logic

--- modal.js
+++ modal.js
@@ -7,21 +7,34 @@ const buttons = document.querySelectorAll(".modal-btn");
 // Get the <span> element that closes the modal
 const span = document.querySelector(".modal-close");
 
+const modalTitle = document.querySelector(".modal-type-char");
+
 // When the user clicks on the button, open the modal
 buttons.forEach(function(button) {
   button.onclick = function() {
+    let titleSpanText;
+
+    if (button.classList.contains("party-btn")) {
+      titleSpanText = document.createTextNode("party character");
+    } else {
+      titleSpanText = document.createTextNode("enemy");
+    }
+
+    modalTitle.appendChild(titleSpanText);
     modal.style.display = "block";
   };
 });
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  modalTitle.innerHTML = "";
   modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target === modal) {
+    modalTitle.innerHTML = "";
     modal.style.display = "none";
   }
 };
